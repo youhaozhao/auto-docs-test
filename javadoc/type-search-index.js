0138b54b8275da482764bb018994f4287add529d
youhaozhao <128596662+youhaozhao@users.noreply.github.com>
2026-02-22 23:13:14 +0000

    Deploying to gh-pages from @ youhaozhao/auto-docs-test@aa2ce9dc4aab920d2d97206e0d1be4c083800830 🚀

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.code","l":"City"},{"p":"com.example.code","l":"CityList"},{"p":"com.example.code","l":"MainActivity"}];updateSearchResults();
